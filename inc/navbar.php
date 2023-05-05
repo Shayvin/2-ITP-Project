e279c52f880f1e916e59d491b811986cf8e01a0f
Shayvin <dominik.leitner@live.de>
2023-05-05 16:43:09 +0200

    Kassa-Front-End & Logo

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php?site=home">Component Corner</a>
+    <a class="navbar-brand" href="index.php?site=home"><img src="res/img/logo.jpg" width="30px" height="30px"> Component Corner</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
