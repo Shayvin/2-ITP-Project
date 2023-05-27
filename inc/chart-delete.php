6a6363a14310b5b3fc3930408274234f117594b5
Matthias Kemmer <mattkem@outlook.de>
2023-05-27 14:04:15 +0200

    Implement chart delete button

---
+++ inc/chart-delete.php
@@ -0,0 +1,20 @@
+<?php
+require_once("./config/dbaccess.php"); // db access
+if (isset($_SESSION["username"]) && isset($_GET["pid"])){
+  $pid = $_GET["pid"];
+  // Get user_id
+  $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :name");
+  $stmt->execute(array(":name" => $_SESSION["username"]));
+  $row = $stmt->fetch();
+  $user_id = $row["ID"];
+  if ($user_id){
+    // check if product is in chart
+    $stmt2 = $mysql->prepare("DELETE FROM warenkorb WHERE user_id = :uid AND artikel_id = :pid");
+    $stmt2->execute(array(":uid" => $user_id, ":pid" => $pid));
+  }
+}
+  // redirect to chart
+  echo '<script type="text/javascript">
+  window.location = "http://localhost/2-ITP-Project/index.php?site=chart"
+  </script>';
+?>
